0bc97fc1118877af4960bd6519b0de4ad4e61424
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-06-05 17:06:07 -0400

    Refactor API response handling to parse JSON instead of raw text

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ load_dotenv()  # Load variables from .env
 api_key = os.getenv("NEWS_API_KEY")
 url = f"https://newsapi.org/v2/everything?q=tesla&from=2025-05-05&sortBy=publishedAt&apiKey={api_key}"
 
+# Make a request to the News API
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary from the response
+content = request.json()
+
+# Access the articles in the response
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
